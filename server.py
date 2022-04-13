41d6053f2b49e494225a1ae3c8c836e17c9b4810
Jonny Saykosy <josayko@pm.me>
2022-04-13 16:34:58 +0200

    Fixes OpenClassrooms-Student-Center/Python_Testing#01

--- server.py
+++ server.py
@@ -28,7 +28,12 @@ def index():
 
 @app.route("/showSummary", methods=["POST"])
 def showSummary():
-    club = [club for club in clubs if club["email"] == request.form["email"]][0]
+    if request.form["email"] == "":
+        return render_template("index.html", error="empty email field"), 400
+    try:
+        club = [club for club in clubs if club["email"] == request.form["email"]][0]
+    except IndexError:
+        return render_template("index.html", error="unknown email"), 400
     return render_template("welcome.html", club=club, competitions=competitions)
 
 
@@ -37,7 +42,9 @@ def book(competition, club):
     foundClub = [c for c in clubs if c["name"] == club][0]
     foundCompetition = [c for c in competitions if c["name"] == competition][0]
     if foundClub and foundCompetition:
-        return render_template("booking.html", club=foundClub, competition=foundCompetition)
+        return render_template(
+            "booking.html", club=foundClub, competition=foundCompetition
+        )
     else:
         flash("Something went wrong-please try again")
         return render_template("welcome.html", club=club, competitions=competitions)
@@ -45,7 +52,9 @@ def book(competition, club):
 
 @app.route("/purchasePlaces", methods=["POST"])
 def purchasePlaces():
-    competition = [c for c in competitions if c["name"] == request.form["competition"]][0]
+    competition = [c for c in competitions if c["name"] == request.form["competition"]][
+        0
+    ]
     club = [c for c in clubs if c["name"] == request.form["club"]][0]
     placesRequired = int(request.form["places"])
     competition["numberOfPlaces"] = int(competition["numberOfPlaces"]) - placesRequired
