4c6f2466385b40ee0eee5825e53dc02275ddcbd5
Jonny Saykosy <josayko@pm.me>
2022-04-13 13:27:41 +0200

    Add flake8 + black config. Update unit tests for index page

--- tests/unit_test/conftest.py
+++ tests/unit_test/conftest.py
@@ -1,9 +1,10 @@
 import pytest
 import server
 
+
 @pytest.fixture
 def client():
-    server.app.config['TESTING'] = True
+    server.app.config["TESTING"] = True
     client = server.app.test_client()
 
     yield client
--- tests/unit_test/unit_test.py
+++ tests/unit_test/unit_test.py
@@ -7,17 +7,36 @@ EMPTY_EMAIL = ""
 UNKNOWN_COMPETITION = "test"
 UNKNOWN_CLUB = "test"
 
-class TestDatabase():
+
+class TestDatabase:
     def test_loadClubs(self):
         """
-        Verify that clubs data are loaded correctly
+        Test that clubs data are loaded correctly
         """
         clubs_list = server.loadClubs()
         assert len(clubs_list) == CLUBS_NB
-    
+
     def test_loadCompetitions(self):
         """
-        Verify that competitions data are loaded correctly
+        Test that competitions data are loaded correctly
         """
         competitions_list = server.loadCompetitions()
         assert len(competitions_list) == COMPETITIONS_NB
+
+
+class TestIndex:
+    def test_should_return_status_code_200(self, client):
+        """
+        Test that index page is loaded correctly
+        """
+        response = client.get("/")
+        assert response.status_code == 200
+
+    def test_should_return_expected_content(self, client):
+        """
+        Test that index page is loaded correctly
+        """
+        response = client.get("/")
+        data = response.data
+        expected_data = b'<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>GUDLFT Registration</title>\n</head>\n<body>\n    <h1>Welcome to the GUDLFT Registration Portal!</h1>\n    Please enter your secretary email to continue:\n    <form action="showSummary" method="post">\n        <label for="email">Email:</label>\n        <input type="email" name="email" id=""/>\n        <button type="submit">Enter</button>\n    </form>\n</body>\n</html>'
+        assert data == expected_data
